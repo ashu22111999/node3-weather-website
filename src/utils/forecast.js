a235bf268f70c8caa01c07c1d45f96eac963b797
Ashutosh Singh <zany22nov99@gmail.com>
2021-05-09 16:11:03 +0530

    Detailed weather description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,12 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + ' degrees out. It feels like '+  body.current.feelslike + ' degrees out.')
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + ' degrees out. Humidity is ' + body.current.humidity + '. Wind direction is ' + body.current.wind_dir + '. It feels like '+  body.current.feelslike + ' degrees out.')
         }
-        // console.log(response)
-        // const data = JSON.parse(response.body)
-        // console.log(data.current)
-        // console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + ' degrees out. It feels like '+  response.body.current.feelslike + ' degrees out.')
     })
 }
 
